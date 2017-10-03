0600817859383f8e6a61dc31dd64d3e6e5a17d04
rodel77 <rodel1407@hotmail.com>
2017-10-03 00:03:12 -0500

    Improved build.py

--- build.py
+++ build.py
@@ -9,21 +9,18 @@ print("Building version: "+version)
 print("")
 print("Files >>")
 
-includes = ['.git']
-
 def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
         for file in files:
             pj = os.path.join(root, file)
-            if "git" not in pj and ".py" not in pj:
+            if ".zip" not in pj and "git" not in pj and ".py" not in pj:
                 print(file)
                 ziph.write(pj)
 
 if __name__ == '__main__':
-    ff = "../spigot_graphs_dist_"+version+".zip"
+    ff = "./spigot_graphs_dist_"+version+".zip"
     zipf = zipfile.ZipFile(ff, 'w', zipfile.ZIP_DEFLATED)
     zipdir('./', zipf)
     zipf.close()
-    os.rename(ff, ff[1:])
     print("Build ended...")
