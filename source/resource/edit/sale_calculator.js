74ae5f7caf4874459d9d78d1539d8c2f9759af7b
BuildTools <unconfigured@null.spigotmc.org>
2020-04-25 13:27:16 +0200

    Improves the sale calculator * Uses selected currency now * Shows saves and sales as float, rounded to 2 digits * Better message, if no sale applied * Doesn't overwrite a sale now (uses start price for new applied sales)

--- source/resource/edit/sale_calculator.js
+++ source/resource/edit/sale_calculator.js
@@ -4,18 +4,27 @@ var premium = document.querySelector("#ctrl_resource_category_id").value==="20";
 
 console.debug("[SpigotSales] Premium:", premium);
 
-var salePercent, costInput, notice;
+var salePercent, costInput, currencyInput, notice;
 var saleData;
+var costsBeforeSale; //sale will be calculated by last set price (not with prices in sales)
 
 if(premium){
     costInput = document.querySelector("#ctrl_bdpaygate_price");
+    currencyInput = document.querySelector("#ctrl_bdpaygate_currency");
     //Cuz JS!
     costInput.addEventListener("change", compute);
     costInput.addEventListener("keydown", compute);
     costInput.addEventListener("keyup", compute);
     costInput.addEventListener("paste", compute);
+    currencyInput.addEventListener("change", compute);
+	
+    costInput.addEventListener("change", function(e) {
+		costsBeforeSale = parseFloat(costInput.value);
+	});
+	costsBeforeSale = parseFloat(costInput.value);
     
     console.debug("[SpigotSales] Current resource price:", costInput.value);
+    console.debug("[SpigotSales] Current resource currency:", currencyInput.value);
     var paymentSection = document.querySelectorAll(".mainContent fieldset")[1];
     // console.log(paymentSection.children.pop())
     paymentSection.insertBefore(createSaleElement(), paymentSection.children[1]);
@@ -67,10 +76,9 @@ function createSaleElement(){
 
 function compute(){
     // Source: http://www.webmath.com/sale.html
-    var cp = parseInt(costInput.value);
-    var sp = parseInt(salePercent.value);
+    var sp = parseFloat(salePercent.value);
     notice.innerText = "";
-    if(isNaN(sp) || isNaN(cp)){
+    if(isNaN(sp) || isNaN(costsBeforeSale)){
         notice.style = "color:red;"
         notice.innerText = "Invalid percentage/cost";
         return false;
@@ -82,11 +90,24 @@ function compute(){
         return false;
     }
 
-    saleData = sale(cp, sp);
+    saleData = sale(costsBeforeSale, sp);
+
+	//get currency and replace some values with currency symbols
+	var cur = getCurrency();
 
     notice.style = "color:black;"
-    notice.innerText = "-"+sp+"%, buyers will save $"+saleData.save+" and the final price will be $"+saleData.sale;
+	if(sp == 0) notice.innerText = "No sale applied.";
+    else notice.innerText = "-"+sp+"%, buyers will save "+saleData.save+cur+" and the final price will be "+saleData.sale+cur;
     return true;
+}
+
+function getCurrency() {
+	var cur = currencyInput.value.toUpperCase();
+	if(cur.localeCompare("EUR") == 0) cur = "€";
+	else if(cur.localeCompare("USD") == 0) cur = "$";
+	else cur = " " + cur;
+	
+	return cur;
 }
 
 function updatePrice(){
@@ -95,9 +116,13 @@ function updatePrice(){
     }
 }
 
+function round(num) {
+	return Math.round(num * 100) / 100
+}
+
 function sale(price, off){
-    var save = (off/100)*price;
-    var sale = price-save;
+    var save = round((off/100)*price);
+    var sale = round(price-save);
 
     console.debug("[SpigotSales] Price:", price, ", Off:", off, ", Save: ", save, ", Final Price: ", sale);
 
